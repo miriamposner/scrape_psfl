dacee942ebd5d41967447c32e872f3cab61468b2
Miriam Posner <miriam.posner@gmail.com>
2015-10-28 21:12:49 -0700

    write to csv

--- psfl-scrape.py
+++ psfl-scrape.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import re
+import csv
 
 html = urlopen("http://silentera.com/PSFL/data/W/WithinOurGates1920.html")
 bsObj = BeautifulSoup(html)
@@ -13,15 +13,15 @@ title = str(title) + ', '
 
 paragraphs = bsObj.find_all('p')
 
-print(title)
-
 for p in paragraphs:
     if "Cast" in str(p):
         p = p.get_text()
         cast = str(p)
         cast = cast.replace("Cast: ","")
-        print(cast)
+        print("ok")
 
 
 
-
+with open('cast_list.csv', 'w') as csvfile:
+    f = csv.writer(csvfile, delimiter=' ')
+    f.writerow([title, cast])
